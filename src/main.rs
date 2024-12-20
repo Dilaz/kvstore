fc5754f4f0a3d1b5b09dbdbfaf12b7e45ff2f2a1
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2024-12-20 12:51:40 +0200

    Update crates and fix some clippy issues

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use axum::{
 use axum_macros::debug_handler;
 use redis::{aio::ConnectionManager, ToRedisArgs};
 use serde::Deserialize;
+use thiserror::Error;
 use std::net::{SocketAddr, Ipv4Addr};
 use tower_http::compression::CompressionLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -11,8 +12,16 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 const REDIS_TOKENS_TABLE: &str = "tokens";
 const PORT: u16 = 3000;
 
+#[derive(Debug, Error)]
+enum KVStoreError {
+    #[error(transparent)]
+    RedisError(#[from] redis::RedisError),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), KVStoreError> {
     tracing::info!("Starting!");
     // initialize tracing
     tracing_subscriber::registry()
@@ -28,18 +37,18 @@ async fn main() {
     // Get client url from env
     let client = match redis::Client::open(std::env::var("REDIS_URL").unwrap_or("redis://127.0.0.1:6379".to_string())) {
         Ok(client) => client,
-        Err(e) => {
-            tracing::error!("Failed to connect to redis: {}", e);
-            return;
+        Err(err) => {
+            tracing::error!("Failed to connect to redis: {}", err);
+            return Err(KVStoreError::RedisError(err));
         }
     };
 
     tracing::info!("Creating connection manager..");
     let connection_manager = ConnectionManager::new(client).await;
 
-    if connection_manager.is_err() {
-        tracing::error!("Failed to connect to redis: {}", connection_manager.err().unwrap());
-        return;
+    if let Err(err) = connection_manager {
+        tracing::error!("Failed to connect to redis: {}", err);
+        return Err(KVStoreError::RedisError(err));
     }
 
     let connection_manager = connection_manager.unwrap();
@@ -51,14 +60,19 @@ async fn main() {
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, PORT));
     tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    match axum::serve(listener, app).await {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            tracing::error!("Failed to start server: {}", err);
+            Err(KVStoreError::IoError(err))
+        }
+    }
 }
 
 fn app(connection_manager: &ConnectionManager) -> Router {
-    return Router::new()
+    Router::new()
     // `GET /` goes to `root`
     .route("/healthz", get(healthcheck))
     .route("/:key",
@@ -68,7 +82,7 @@ fn app(connection_manager: &ConnectionManager) -> Router {
         .layer(from_fn_with_state(connection_manager.clone(), get_prefix_by_token))
     )
     .layer(CompressionLayer::new())
-    .with_state(connection_manager.clone());
+    .with_state(connection_manager.clone())
 }
 
 // basic handler that responds with a static string
@@ -82,15 +96,13 @@ async fn get_key(
     tracing::info!("GET {}", key);
     if let Ok(resp) = conn.send_packed_command(redis::cmd("GET").arg(key.to_redis_args())).await {
         match resp {
-            redis::Value::Nil => return (StatusCode::NOT_FOUND, Json("Key not found".to_string())),
-            redis::Value::Data(str) => {
-                let resp = String::from_utf8(str).unwrap();
-                return (StatusCode::OK, Json(resp));
-            }
-            _ => {}
+            redis::Value::Nil => (StatusCode::NOT_FOUND, Json("Key not found".to_string())),
+            redis::Value::SimpleString(str) => (StatusCode::OK, Json(str.to_string())),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string())),
         }
+    } else {
+        (StatusCode::NOT_FOUND, Json("Key not found".to_string()))
     }
-    (StatusCode::NOT_FOUND, Json("Key not found".to_string()))
 }
 
 async fn post_value(
@@ -102,10 +114,10 @@ async fn post_value(
     let key = format!("{}:{}", ext, key);
     tracing::info!("SET {}", key);
     match conn.send_packed_command(redis::cmd("SET").arg(key.to_redis_args()).arg(payload.value.to_redis_args())).await {
-        Ok(_) => return (StatusCode::OK, Json("Ok".to_string())),
+        Ok(_) => (StatusCode::OK, Json("Ok".to_string())),
         Err(err) => {
             tracing::error!("Failed to set key: {}", err);
-            return (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()))
+            (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()))
         },
 
     }
@@ -119,39 +131,39 @@ async fn delete_key(
     let key = format!("{}:{}", ext, key);
     tracing::info!("DELETE {}", key);
     match conn.send_packed_command(redis::cmd("DEL").arg(key.to_redis_args())).await {
-        Ok(redis::Value::Okay) => return (StatusCode::OK, Json("Ok".to_string())),
-        Ok(_) => return (StatusCode::OK, Json("OK".to_string())),
+        Ok(redis::Value::Okay) => (StatusCode::OK, Json("Ok".to_string())),
+        Ok(_) => (StatusCode::OK, Json("OK".to_string())),
         Err(err) => {
             tracing::error!("Failed to set key: {}", err);
-            return (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()));
+            (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()))
         },
     }
 }
 
 async fn healthcheck(State(mut conn): State<ConnectionManager>) -> (StatusCode, Json<String>) {
     match conn.send_packed_command(&redis::cmd("PING")).await {
-        Ok(redis::Value::Status(val)) if val == "PONG" => return (StatusCode::OK, Json("Ok".to_string())),
+        Ok(redis::Value::SimpleString(str)) if str == "PONG" => (StatusCode::OK, Json("Ok".to_string())),
         Ok(value) => { 
             tracing::error!("Status check failed: {:?}", value);
-            return (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()))
+            (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()))
         },
         Err(err) => {
             tracing::error!("Status check failed: {}", err);
-            return (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()));
+            (StatusCode::INTERNAL_SERVER_ERROR, Json("Internal Server Error".to_string()))
         },
     }
 }
 
-async fn get_prefix_by_token<B>(
+async fn get_prefix_by_token(
     State(mut conn): State<ConnectionManager>,
     headers: HeaderMap,
-    request: Request<B>,
-    next: Next<B>,
+    request: Request<axum::body::Body>,
+    next: Next,
 ) -> Result<Response, StatusCode> {
     if let Some(authorize_header) = headers.get(header::AUTHORIZATION) {
         let token: String = authorize_header.to_str().unwrap_or("").split(" ").last().unwrap_or("").to_string();
-        match conn.send_packed_command(redis::cmd("SISMEMBER").arg(REDIS_TOKENS_TABLE.to_redis_args()).arg(&token.to_redis_args())).await {
-            Ok(redis::Value::Int(n)) if n == 1 => {
+        match conn.send_packed_command(redis::cmd("SISMEMBER").arg(REDIS_TOKENS_TABLE.to_redis_args()).arg(token.to_redis_args())).await {
+            Ok(redis::Value::Int(1)) => {
                 let mut request = request;
                 request.extensions_mut().insert(token);
                 return Ok(next.run(request).await)
@@ -165,7 +177,7 @@ async fn get_prefix_by_token<B>(
 }
 
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct SetValue {
     value: String,
 }
